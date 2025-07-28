22b24c02c74f85c4ebd09daa27d3284e420f62f1
SHIVAM BARANWAL <98750358+Shivambaranwal267@users.noreply.github.com>
2025-07-28 16:33:57 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:ecommerce_app/controllers/auth_controller.dart';
+import 'package:ecommerce_app/controllers/theme_controller.dart';
+import 'package:ecommerce_app/utils/app_themes.dart';
+import 'package:ecommerce_app/view/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async {
+  await GetStorage.init();
+  Get.put(ThemeController());
+  Get.put(AuthController());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final themeController = Get.find<ThemeController>();
+    return GetMaterialApp(
+      title: 'Ecommerce Store',
+      debugShowCheckedModeBanner: false,
+      theme: AppThemes.light,
+      darkTheme: AppThemes.dark,
+      themeMode: themeController.theme,
+      defaultTransition: Transition.fade,
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/view/onboarding_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  final List<OnboardingItem> _items = [
+    OnboardingItem(
+      description:
+          'Explore the newest fashion trends and find your unique style',
+      title: 'Discover Latest Trends',
+      image: 'assets/images/intro.png',
+    ),
+    OnboardingItem(
+      description: 'Shop premium quality products from top brands worldwide',
+      title: 'Quality Products',
+      image: 'assets/images/intro1.png',
+    ),
+    OnboardingItem(
+      description: 'Simple and secure shopping experience at your fingertips',
+      title: 'Easy SHopping',
+      image: 'assets/images/intro2.png',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            itemCount: _items.length,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Image.asset(_items[index].image)],
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnboardingItem {
+  final String image;
+  final String title;
+  final String description;
+
+  OnboardingItem({
+    required this.description,
+    required this.image,
+    required this.title,
+  });
+}
